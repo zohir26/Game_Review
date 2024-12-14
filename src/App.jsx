86f677c71c3c1c88414ae1d1243f4e52db77ec21
zohir <jahirraihan500@gmail.com>
2024-12-14 07:26:17 +0600

    installation

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    
+      <div>
+        <h1>Game Review</h1>
+
+        <p className="read-the-docs">
+        This is game reviewing single page application.
+        </p>
+
+      </div>
+
+  )
+}
+
+export default App
